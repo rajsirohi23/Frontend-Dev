80ef04ba64dcc398b6a62014cb265102701e7480
Raj Sirohi <rajsirohi2555@gmail.com>
2025-11-19 14:01:23 +0530

    Raj Sirohi has added js basics code

---
+++ personalized_login.js
@@ -0,0 +1,16 @@
+let userName= "Ekta";
+let currentHour= new Date().getHours();
+
+
+
+if (currentHour<12) {
+    console.log(`Good Morning ${userName}`)
+    
+} 
+else if(currentHour>12 && currentHour<17){
+    console.log(`Good Afternoon ${userName}`)
+}
+else{
+    console.log(`Good Evening ${userName}`)
+    
+}
